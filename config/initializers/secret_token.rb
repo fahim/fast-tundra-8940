a9ef81ba00104936474ee21850693a3933dd3259
Fahim Ferdous <thefahim@gmail.com>
2014-01-18 12:46:31 -0800

    new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstacartCohortAnalysis::Application.config.secret_token = 'd5c6977d991eee7b8b16ddefcb88047f015eb332dd8d3e52fcdec08a75540ab41f891694c817ce65b9198ea597c1c131eec7ab8399f62d4ff9e078255426872c'
